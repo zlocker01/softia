5fb04f1d59955eb7479c8983fdde61695d7de879
Diego Beristain <beristaindelamora@gmail.com>
2024-10-24 16:57:02 -0600

    refactor: :recycle: refactoring calendar

--- src/components/agenda/Calendar.tsx
+++ src/components/agenda/Calendar.tsx
@@ -17,13 +17,8 @@ export const MyCalendar = () => {
     setCurrentDate(newDate);
   };
 
-  const formattedDate = dayjs(currentDate)
-    .format('D [de] MMMM [de] YYYY')
-    .toUpperCase();
-
   return (
     <div className="w-full h-dvh">
-      <h2 className="text-center text-xl font-bold mb-4">{formattedDate}</h2>
       <Calendar
         localizer={localizer}
         events={[]}
@@ -31,8 +26,8 @@ export const MyCalendar = () => {
         views={['month', 'week', 'day', 'agenda']}
         view={currentView}
         onView={(view) => setCurrentView(view)}
-        date={currentDate} // Aquí pasamos el currentDate correcto
-        onNavigate={handleNavigate} // Aquí usamos la función para navegar
+        date={currentDate}
+        onNavigate={handleNavigate}
         step={60}
         showMultiDayTimes
         messages={{
@@ -47,32 +42,24 @@ export const MyCalendar = () => {
           noEventsInRange: 'No hay citas registradas',
         }}
         formats={{
-          // Encabezado de día en las vistas de 'week' y 'day'
           dayHeaderFormat: (date) =>
             dayjs(date).format('dddd DD [de] MMMM [de] YYYY'), // Ejemplo: "Lunes 23 de Octubre de 2024"
 
-          // Encabezado de la vista de 'month'
           monthHeaderFormat: (date) => dayjs(date).format('MMMM [de] YYYY'), // Ejemplo: "Octubre de 2024"
 
-          // Título de cada evento en las vistas de 'week' y 'day'
           agendaDateFormat: (date) => dayjs(date).format('dddd DD [de] MMMM'), // Ejemplo: "Lunes 23 de Octubre"
 
-          // Formato para los eventos en la vista de 'agenda'
           agendaTimeRangeFormat: ({ start, end }) =>
             `${dayjs(start).format('HH:mm')} - ${dayjs(end).format('HH:mm')}`, // Ejemplo: "10:00 - 11:00"
 
-          // Formato para el rango de fechas en el encabezado de la vista 'agenda'
           agendaHeaderFormat: ({ start, end }) =>
             `${dayjs(start).format('D [de] MMMM [de] YYYY')} - ${dayjs(end).format('D [de] MMMM [de] YYYY')}`, // Ejemplo: "23 de Octubre de 2024 - 29 de Octubre de 2024"
 
-          // Formato para la barra superior que muestra el rango de fechas en la vista de 'week'
           dayRangeHeaderFormat: ({ start, end }) =>
             `${dayjs(start).format('D [de] MMMM')} - ${dayjs(end).format('D [de] MMMM')}`, // Ejemplo: "23 de Octubre - 29 de Octubre"
 
-          // Formato de la fecha de cada día en el encabezado de las columnas en la vista 'week'
           dayFormat: (date) => dayjs(date).format('ddd DD/MM'), // Ejemplo: "Lun 23/10"
 
-          // Formato para la vista de 'agenda' para el día y la hora del evento
           timeGutterFormat: (date) => dayjs(date).format('HH:mm'), // Ejemplo: "10:00"
         }}
       />
